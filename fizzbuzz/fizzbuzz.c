999fc4ec412d5e3316fd0e0e2503830588555943
Wanda <doubleuevans@gmail.com>
2019-04-29 14:53:15 -0500

    complete FizzBuzz module

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,25 @@
 */
 int fizzbuzz(int n)
 {
+    int i;
+    for (i=1; i<=n; i++)
+    {
+        if (i % 15 == 0)
+            printf("FizzBuzz\n");
 
+        else if(i % 3 == 0)
+            printf("Fizz\n");
+        
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+
+        else
+            printf("%i\n", i); //%i is for integers
+    }
+    
+    //my function main returns an int so I should have a return at the end of it
+    return 0; 
+    
 }
 
 #ifndef TESTING
